107e7ff9eeebc76df198ca1c4ad98dea37207328
Fabian Calangiu <fabian.calangiu@studio.unibo.it>
2025-11-04 11:07:33 +0100

    Added instance of custom comparator and empty TreeSet

--- 02-custom-comparator/src/main/java/it/unibo/collections/comparators/UseSetWithOrder.java
+++ 02-custom-comparator/src/main/java/it/unibo/collections/comparators/UseSetWithOrder.java
@@ -2,6 +2,8 @@ package it.unibo.collections.comparators;
 
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.Collections;
+import java.util.Comparator;
 
 /**
  * 
@@ -25,6 +27,10 @@ public final class UseSetWithOrder {
          * The comparator must convert the strings to double, then compare the doubles to find the biggest.
          * The comparator does not need to deal with the case of Strings which are not parseable as doubles.
          */
+        Comparator<String> ValueComparator = new SortByValue();
+        TreeSet<String> setOfStrings = new TreeSet<>(ValueComparator);
+
+        System.out.println("Sorted TreeSet by custom comparator: " + setOfStrings);
         /*
          * 2) Inserts in the set a hundred strings containing randomly generated numbers
          * (use Math.random(), and convert to String appropriately)
